fdfb6349b513029dfeae160f3608e21b61d1dead
Ruslan Gokhman <61827720+Gokhman07@users.noreply.github.com>
2021-01-04 17:57:19 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ updater=Updater(TG_TOKEN,use_context=True)
 updater.start_webhook(listen="0.0.0.0",
 port=int(PORT),
 url_path=TG_TOKEN)
-updater.bot.setWebhook('https://onatschedule.herokuapp.com/' + TG_TOKEN)
+updater.bot.setWebhook('https://vegasopros.herokuapp.com/' + TG_TOKEN)
 
 
 
